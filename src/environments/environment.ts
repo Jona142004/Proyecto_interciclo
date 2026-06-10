5d9c178ef8c9d40d1f21ac298402c36870022f24
Jona142004 <jonnathanhola17@gmail.com>
2026-06-10 02:28:32 -0500

    antes de la implementacion

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB0dSYMKTs_UpB_O2wZx7rmAszkC9xODGQ",
+    authDomain: "portafolio-servicios-ac106.firebaseapp.com",
+    projectId: "portafolio-servicios-ac106",
+    storageBucket: "portafolio-servicios-ac106.firebasestorage.app",
+    messagingSenderId: "10813113239",
+    appId: "1:10813113239:web:a501ce351da51fde903ad2"
+  },
+  strapiUrl: "http://localhost:1337"
+};
